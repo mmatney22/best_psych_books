68befbb7dbb0b307c36c4056d8ef6ad3e4701d4d
Morgan Matney <morgan.matney89@gmail.com>
2020-10-05 13:29:45 +0000

    setup initial files

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,7 +1,7 @@
-require "psych_books/version"
-require_relative "psych_books/cli"
-require_relative "psych_books/book"
-require_relative "psych_books/scraper"
+require_relative "./psych_books/version"
+require_relative "./psych_books/cli"
+require_relative "./psych_books/book"
+require_relative "./psych_books/scraper"
 
 module PsychBooks
   class Error < StandardError; end
