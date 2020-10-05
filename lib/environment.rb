65cdb4c0d55902c8c7fea988ae528d3524050f82
Morgan Matney <morgan.matney89@gmail.com>
2020-10-05 13:15:18 +0000

    create environment doc

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,4 +1,7 @@
 require "psych_books/version"
+require_relative "psych_books/cli"
+require_relative "psych_books/book"
+require_relative "psych_books/scraper"
 
 module PsychBooks
   class Error < StandardError; end
