55da6c5d6927ac8c2891f1588c6db3a2151ef0dc
Morgan Matney <morgan.matney89@gmail.com>
2020-10-05 12:53:12 +0000

    add description

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in psych_books.gemspec
+gemspec
---
+++ lib/psych_books.rb
@@ -0,0 +1,6 @@
+require "psych_books/version"
+
+module PsychBooks
+  class Error < StandardError; end
+  # Your code goes here...
+end
