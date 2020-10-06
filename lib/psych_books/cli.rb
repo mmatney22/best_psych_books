072c77bad1ba9501df83d123bf997feddcd0d41b
Morgan Matney <morgan.matney89@gmail.com>
2020-10-06 21:37:23 +0000

    remove comments

--- lib/psych_books/cli.rb
+++ lib/psych_books/cli.rb
@@ -8,26 +8,26 @@ class PsychBooks::CLI
   end
   
   def get_books
-    @books = PsychBooks::Book.all             #sets variable to all array, containing book objects
+    @books = PsychBooks::Book.all             
   end
   
   def list_books
     puts "\nChoose a book to see more information:\n".green
-    @books.each.with_index(1) {|book, index| puts "#{index}. #{book.title}".cyan}     #iterates and lists books
+    @books.each.with_index(1) {|book, index| puts "#{index}. #{book.title}".cyan}     
   end
   
   def get_user_book
     chosen_book = gets.strip.to_i
-    list_info_for(chosen_book) if valid_input?(chosen_book, @books)        #if valid input, show list of books
+    list_info_for(chosen_book) if valid_input?(chosen_book, @books)        
   end
   
   def valid_input?(input, data)
-    input.to_i <= data.length && input.to_i > 0        #change input to integar and check validity
+    input.to_i <= data.length && input.to_i > 0        
   end
   
   def list_info_for(chosen_book)
     book = @books[chosen_book - 1] 
-    PsychBooks::Scraper.scrape_book_details(book)   #set variable to array item of @books, minus 1 for correct index
+    PsychBooks::Scraper.scrape_book_details(book)   
     puts "\nHere are the details for #{book.title}:\n".green
     puts "\nAuthor:".yellow.bold + " #{book.author}\n".cyan
     puts "\nDescription:".yellow.bold + " #{book.description}\n".cyan
